89210ab66e5b67674190ae9f78ade18c7fd60eeb
Petros <petros.rodinos@yahoo.com>
2026-04-02 16:32:57 +0300

    fodler structure

--- dist/cli.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./cli/app");
-(0, app_1.startCli)().catch((error) => {
-    const message = error instanceof Error ? error.message : String(error);
-    console.error(message);
-    process.exit(1);
-});
--- dist/trippo/types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// ── Primitives & shared enums ────────────────────────────────────────────────
-Object.defineProperty(exports, "__esModule", { value: true });
